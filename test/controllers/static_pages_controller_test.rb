b2058305090a30e0f402094e70687ab47bed3a98
vadymzak <vadim.if@gmail.com>
2019-01-23 10:46:07 +0200

    fix bug with root test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,8 +6,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get root" do
-    get FILL_IN
-    assert_response FILL_IN
+    get root_url
+    assert_response :success
   end
 
   test "should get home" do
